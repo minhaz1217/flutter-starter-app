5a86da5a8e9e6bd0936658efa3de22c301be2907
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-20 15:42:18 +0600

    change page and operations

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: RandomWords(),
+
     );
   }
 }
@@ -62,10 +63,47 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[      // Add 3 lines from here...
+        IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved),
+      ],                      // ... to here.
       ),
       body: _buildSuggestions(),
     );
   }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
+      ),
+    );
+  }
+
 }
 class RandomWords extends StatefulWidget {
   @override
